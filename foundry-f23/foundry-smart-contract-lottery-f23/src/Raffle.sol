b720d55e6cbd4cc9ebd829404352f21873802af2
lmafr <alejandrofloridoreyes@gmail.com>
2025-01-12 13:03:25 +0100

    Added customError to add functionality to enterRaffle function.

--- foundry-f23/foundry-smart-contract-lottery-f23/src/Raffle.sol
+++ foundry-f23/foundry-smart-contract-lottery-f23/src/Raffle.sol
@@ -32,6 +32,10 @@ pragma solidity 0.8.19;
  * @notice  This contract is for creating a sample raffle
  */
 contract Raffle {
+
+    /** Errors */
+    error Raffle__SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
 
     constructor(uint256 entranceFee) {
@@ -39,7 +43,20 @@ contract Raffle {
     }
 
     function enterRaffle() public payable {
-
+        // The line below is cool and can be found in a lot of legacy code, however...
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent");
+        // Nowadays we can use custom errors, which spend less gas (we do not add the string), nevertheless...
+        // if (msg.value < i_entranceFee) {
+        //     revert Raffle__SendMoreToEnterRaffle();
+        // }
+        // Nowadays too, we can add the error directly in the require, which is much more readable:
+        // require(msg.value > i_entranceFee, Raffle__SendMoreToEnterRaffle());
+        // But that last feature is available from 0.8.26 solidity version (higher than the one we are using here), requires compile
+        // via IR (which takes a lot to compile) and it is still less efficient (theoretically) than 2nd version, so we will use the
+        //  2nd version provided in these comments:
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
     }
 
     function pickWinner() public {
